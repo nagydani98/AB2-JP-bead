7f47dce343a75adf96bcdcf73300a54f625ed7fa
nagydani98 <n.dani9812@gmail.com>
2019-03-18 20:59:43 +0100

    openConnection method

--- LibraryHandler/src/controllers/MainController.java
+++ LibraryHandler/src/controllers/MainController.java
@@ -26,13 +26,20 @@ public class MainController {
 	
 	public static void operateInConsole(){
 		ConsoleView consoleView = new ConsoleView();
-		String[] loginDetails = consoleView.requestLoginDetails();
+		String[] loginDetails = consoleView.requestLoginDetails(); //loginDetails[0] is the username and loginDetails[1] is the password
 		openConnection(loginDetails[0], loginDetails[1]);
 		
 	}
 	
 	public static void openConnection(String username, String password) {
-		//TODO
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String url = "jdbc:oracle:thin:@localhost:1521:XE";
+			connection = DriverManager.getConnection(url, username, password);
+			System.out.println("Connection established");
+		} catch (Exception ex) {
+			System.err.println(ex.getMessage());
+		}
 	}
 	
 	public static void closeConnection() {
